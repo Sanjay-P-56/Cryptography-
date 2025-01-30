e896f15d70b16e93072f6939e11189b6c5a9871d
Sanjay P <sanjayprasanna56@gmail.com>
2025-01-30 22:12:15 +0530

    RSA.java

--- RSA.java
+++ RSA.java
@@ -1,85 +1,31 @@
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Scanner;
-
-public class RSA{
-    private static final SecureRandom random = new SecureRandom();
-    private static BigInteger n, e, d;
-
-    public static void generateKeys(int bits) {
-        BigInteger p = BigInteger.probablePrime(bits / 2, random);
-        BigInteger q = BigInteger.probablePrime(bits / 2, random);
-        n = p.multiply(q);
-        BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
-
-        e = BigInteger.probablePrime(bits / 2, random);
-        while (phi.gcd(e).compareTo(BigInteger.ONE) != 0) {
-            e = BigInteger.probablePrime(bits / 2, random);
-        }
-
-        d = e.modInverse(phi);
-
-        System.out.println("\nPublic Key: (e=" + e + ", n=" + n + ")");
-        System.out.println("Private Key: (d=" + d + ", n=" + n + ")");
-    }
-
-    public static BigInteger[] encrypt(String message) {
-        BigInteger[] encrypted = new BigInteger[message.length()];
-        for (int i = 0; i < message.length(); i++) {
-            encrypted[i] = BigInteger.valueOf((int) message.charAt(i)).modPow(e, n);
-        }
-        return encrypted;
-    }
-
-    public static String decrypt(BigInteger[] ciphertext) {
-        StringBuilder decryptedMessage = new StringBuilder();
-        for (BigInteger cipher : ciphertext) {
-            decryptedMessage.append((char) cipher.modPow(d, n).intValue());
-        }
-        return decryptedMessage.toString();
-    }
-
+import java.nio.charset.StandardCharsets;
+class RSA {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        generateKeys(512);
+        SecureRandom random = new SecureRandom();
+        BigInteger p = BigInteger.probablePrime(512, random);
+        BigInteger q = BigInteger.probablePrime(512, random);
+        BigInteger n = p.multiply(q);
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger e = new BigInteger("65537");
+        BigInteger d = e.modInverse(phi);
 
-        while (true) {
-            System.out.println("\nChoose an option:");
-            System.out.println("1. Encrypt a message");
-            System.out.println("2. Decrypt a message");
-            System.out.println("3. Exit");
-            System.out.print("\nEnter your choice (1/2/3): ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a text message to encrypt:");
+        String message = sc.nextLine();
+        BigInteger messageNum = new BigInteger(message.getBytes(StandardCharsets.UTF_8));
 
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+        BigInteger encrypted = messageNum.modPow(e, n);
+        BigInteger decryptedNum = encrypted.modPow(d, n);
+        String decrypted = new String(decryptedNum.toByteArray(), StandardCharsets.UTF_8);
 
-            if (choice == 1) {
-                System.out.print("\nEnter a message to encrypt: ");
-                String message = scanner.nextLine();
-                BigInteger[] encryptedMessage = encrypt(message);
-                System.out.print("\nEncrypted Message: ");
-                for (BigInteger num : encryptedMessage) {
-                    System.out.print(num + " ");
-                }
-                System.out.println();
-            } else if (choice == 2) {
-                System.out.print("\nEnter the encrypted message (space-separated numbers): ");
-                String[] encryptedInput = scanner.nextLine().split(" ");
-                BigInteger[] encryptedMessage = new BigInteger[encryptedInput.length];
+        System.out.println("Public Key (n, e): " + n + ", " + e);
+        System.out.println("Private Key (d): " + d);
+        System.out.println("Encrypted Message: " + encrypted);
+        System.out.println("Decrypted Message: " + decrypted);
 
-                for (int i = 0; i < encryptedInput.length; i++) {
-                    encryptedMessage[i] = new BigInteger(encryptedInput[i]);
-                }
-
-                String decryptedMessage = decrypt(encryptedMessage);
-                System.out.println("\nDecrypted Message: " + decryptedMessage);
-            } else if (choice == 3) {
-                System.out.println("\nExiting the program...");
-                break;
-            } else {
-                System.out.println("\nInvalid choice, please try again.");
-            }
-        }
-        scanner.close();
+        sc.close();
     }
 }
